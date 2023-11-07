d7f429c3c1d29313ff573dc6de5233ebe1ef8f67
abelgarcia4545 <149451272+abelgarcia4545@users.noreply.github.com>
2023-11-06 20:08:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
+    celcius = int(input())
+
+    farenheit = (celcius * 9/5) + 32
+
+    print(f'{farenheit:.2f}')
 
     """
     ########################################
