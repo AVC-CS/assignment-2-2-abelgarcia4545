5e1613f1433a8c373f2e0eedb82eb795f11b5d8b
abelgarcia4545 <149451272+abelgarcia4545@users.noreply.github.com>
2023-11-06 20:14:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     ##################################################
     """
     celsius = int(input())
-    fahrenheit = (celcius * 9/5) + 32
+    fahrenheit = (celsius * 9/5) + 32
     print(f'{fahrenheit:.2f}')
     """
     ########################################
